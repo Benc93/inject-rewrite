9fc82b7762ec52622c9cff630f61e563c232618d
Ben Conway <bendavidconway@me.com>
2014-11-11 13:59:29 +0000

    adding strings and minor refactoring

--- lib/inject_rewrite.rb
+++ lib/inject_rewrite.rb
@@ -1,13 +1,11 @@
 class Array
 
   def jab
-    
-    y       = self.count         #determines array size
-    array   = self.slice(1..y)   #splits up the array into elements
-    starter = self[0]            #sets a starter variable at the 1st element in the array
+    # y       = self.count         #determines array size
+    array   = self.slice(1..-1)   #returns a new array from the second to the last element in the original array
+    starter = self[0]             #sets the starter variable at the 1st element in the array
 
-    array.each do |element| 
-
+    array.map do |element| 
       starter = yield(starter, element) 
     end #sets the starter variable and runs the block maps each element
     starter  #returns the finished accumulator value
--- spec/inject_rewrite_spec.rb
+++ spec/inject_rewrite_spec.rb
@@ -1,6 +1,6 @@
 require 'inject_rewrite'
 
-  context 'inject_rewrite should' do 
+  context 'inject_rewrite (AKA Jab) should' do 
 
     let (:array) {Array.new}
 
@@ -19,5 +19,13 @@ require 'inject_rewrite'
       expect(array.jab {|starter, element| starter * element}).to eq 32
     end
 
+    it 'be able to divide all elements in the array' do 
+      array = [1000, 10, 10]
+      expect(array.jab {|starter, element| starter / element}).to eq 10
+    end
 
+    it 'be able to add string values' do 
+      array = ["Inject ", "is ", "rewritten"]
+      expect(array.jab {|starter, element| starter + element}).to eq "Inject is rewritten"
+    end
   end
