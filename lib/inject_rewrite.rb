7dc99ea0d2e2aee27ef79919b8b0c6f502a36634
Ben Conway <bendavidconway@me.com>
2014-11-13 18:37:26 +0000

    can take arguements & minor refactoring

--- lib/inject_rewrite.rb
+++ lib/inject_rewrite.rb
@@ -1,11 +1,16 @@
 class Array
 
-  def jab
-    # y       = self.count         #determines array size
-    array   = self.slice(1..-1)   #returns a new array from the second to the last element in the original array
-    starter = self[0]             #sets the starter variable at the 1st element in the array
+  def jab(starter = self[0])
 
-    array.map do |element| 
+     if starter == self[0]
+      array   = self.slice(1..-1)    #returns a new array from the second to the last element in the original array
+      # starter = self[0]              #sets the starter variable at the 1st element in the array
+    else 
+      array   = self.slice(0..-1)    #returns a new array from the second to the last element in the original array
+      # starter = start_value   
+     end
+
+      array.map do |element| 
       starter = yield(starter, element) 
     end #sets the starter variable and runs the block maps each element
     starter  #returns the finished accumulator value
--- spec/inject_rewrite_spec.rb
+++ spec/inject_rewrite_spec.rb
@@ -6,7 +6,7 @@ require 'inject_rewrite'
 
     it 'be able to sum all elements in an array' do
       array = [5, 6, 7, 8]
-      expect(array.jab {|starter, element| starter + element}).to eq 26
+      expect(array.jab(3) {|starter, element| starter + element}).to eq 29
     end  
 
     it 'be able to subtract all elements in the array' do 
